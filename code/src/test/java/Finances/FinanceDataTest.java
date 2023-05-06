a3ee167f1e15d1431910f0ff0a84dec433161c9b
Dániel Földes <foldesdaniel9@gmail.com>
2023-05-06 13:14:30 +0200

    add finances tests

---
+++ code/src/test/java/Finances/FinanceDataTest.java
@@ -0,0 +1,15 @@
+package Finances;
+
+import org.junit.jupiter.api.Test;
+import simplicity.Model.Finances.FinanceData;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FinanceDataTest {
+    @Test
+    public void testToString() {
+        FinanceData data = new FinanceData(1000, "ASD");
+        assertTrue(data.toString(true).equals("+$1000 ASD"));
+        assertTrue(data.toString(false).equals("-$1000 ASD"));
+    }
+}
---
+++ code/src/test/java/Finances/FinanceTest.java
@@ -0,0 +1,91 @@
+package Finances;
+
+import org.junit.jupiter.api.Test;
+import simplicity.Model.Finances.Finance;
+import simplicity.Model.Person.Person;
+import simplicity.Model.Placeables.Zones.Residential;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FinanceTest {
+    @Test
+    public void testFinanceTestConstructor() {
+        Finance finance = new Finance(1000);
+        assertTrue(finance.getCurrentWealth() == 1000);
+    }
+
+    @Test
+    public void testRemoveMoney() {
+        Finance finance = new Finance(1000);
+        finance.removeMoney(500);
+        assertTrue(finance.getCurrentWealth() == 500);
+    }
+
+    @Test
+    public void testAddMoney() {
+        Finance finance = new Finance(1000);
+        finance.addMoney(500);
+        assertTrue(finance.getCurrentWealth() == 1500);
+    }
+
+    @Test
+    public void testAddIncome() {
+        Finance finance = new Finance(1000);
+        finance.addIncome(2000, "ASD");
+        assertTrue(finance.getIncomeList().size() == 1);
+        assertTrue(finance.getIncomeList().get(0).getPrice() == 2000);
+        assertTrue(finance.getIncomeList().get(0).getText().equals("ASD"));
+    }
+
+    @Test
+    public void testAddBuilt() {
+        Finance finance = new Finance(1000);
+        finance.addBuilt(2000, "ASD");
+        assertTrue(finance.getBuiltList().size() == 1);
+        assertTrue(finance.getBuiltList().get(0).getPrice() == 2000);
+        assertTrue(finance.getBuiltList().get(0).getText().equals("ASD"));
+    }
+
+    @Test
+    public void testAddYearlySpend() {
+        Finance finance = new Finance(1000);
+        finance.addYearlySpend(2000, "ASD");
+        assertTrue(finance.getYearlySpendList().size() == 1);
+        assertTrue(finance.getYearlySpendList().get(0).getPrice() == 2000);
+        assertTrue(finance.getYearlySpendList().get(0).getText().equals("ASD"));
+    }
+
+    @Test
+    public void testRemoveYearlySpend() {
+        Finance finance = new Finance(1000);
+        finance.addYearlySpend(2000, "ASD");
+        finance.removeYearlySpend(2000, "ASD");
+        assertTrue(finance.getYearlySpendList().size() == 0);
+    }
+
+    @Test
+    public void testBuiltToString() {
+        Finance finance = new Finance(5000);
+        finance.addBuilt(1000, "ASD1");
+        finance.addBuilt(2000, "ASD2");
+        assertTrue(finance.builtToString().equals("-$1000 ASD1\n-$2000 ASD2\n"));
+    }
+
+    @Test
+    public void testYearlySpendToString() {
+        Finance finance = new Finance(5000);
+        finance.addYearlySpend(1000, "ASD1");
+        finance.addYearlySpend(2000, "ASD2");
+        assertTrue(finance.yearlySpendToString().equals("-$1000 ASD1\n-$2000 ASD2\n"));
+    }
+
+    @Test
+    public void testIncomeToString() {
+        Finance finance = new Finance(5000);
+        finance.addIncome(1000, "ASD1");
+        finance.addIncome(2000, "ASD2");
+        assertTrue(finance.incomeToString().equals("+$1000 ASD1\n+$2000 ASD2\n"));
+    }
+}
